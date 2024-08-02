e710cf8241b1b30384be4ba13b43a7392614ca76
NAERNON <naernon@icloud.com>
2024-08-03 00:23:35 +0200

    Initial commit

---
+++ Source/Godot Class.xctemplate/Object/___VARIABLE_fileName___.cpp
@@ -0,0 +1,14 @@
+//___FILEHEADER___
+
+#include "___VARIABLE_fileName___.hpp"
+
+using namespace godot;
+
+___VARIABLE_productName___::___VARIABLE_productName___() {}
+___VARIABLE_productName___::~___VARIABLE_productName___() {}
+
+void ___VARIABLE_productName___::_bind_methods() {}
+
+// MARK: Setup
+
+void ___VARIABLE_productName___::_ready() {}
---
+++ Source/Godot Class.xctemplate/Object/___VARIABLE_fileName___.hpp
@@ -0,0 +1,24 @@
+//___FILEHEADER___
+
+#ifndef ___VARIABLE_fileName____hpp
+#define ___VARIABLE_fileName____hpp
+
+#include <godot_cpp/classes/___VARIABLE_superclassFileName___.hpp>
+
+using namespace godot;
+
+class ___VARIABLE_productName___ : public ___VARIABLE_godotSuperclass___ {
+    GDCLASS(___VARIABLE_productName___, ___VARIABLE_godotSuperclass___)
+    
+protected:
+    static void _bind_methods();
+    
+public:
+    ___VARIABLE_productName___();
+    ~___VARIABLE_productName___();
+    
+public: // MARK: Setup
+    void _ready() override;
+};
+
+#endif /* ___VARIABLE_fileName____hpp */
